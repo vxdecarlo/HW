1d82f706fa151fd3c903da31661d46812227da12
Vincent DeCarlo <vxdecarlo@gmail.com>
2015-10-01 11:54:37 -0500

    CrazyMath's first version

---
+++ CrazyMath/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrazyMath
+{
+    class Math
+    {
+        public int DoSomething(int a, int b) {
+            return a/b;
+        }
+        public int DoSomething(int a, int b, int c)
+        {
+            return a+b+c;
+        }
+        public int DoSomething(int a, int b, int c, int d)
+        {
+            return a*b*c*d;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math m = new Math();
+        }
+    }
+}
